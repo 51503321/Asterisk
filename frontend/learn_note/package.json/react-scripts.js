a34beecb3f046524b13544305a5bdebcc3cb49fd
Thien <33951318+51503321@users.noreply.github.com>
2025-04-22 16:24:59 +0700

    feat(ATR): init my-app src

---
+++ frontend/learn_note/package.json/react-scripts.js
@@ -0,0 +1,2 @@
+// create-react-app is a global command-line utility that you use to create new projects.
+// react-scripts is a development dependency in the generated projects (including this one).
