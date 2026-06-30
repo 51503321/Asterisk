89ee413cf5e3361a1ce9a89f26fa0d9002f7baaa
Thien <33951318+51503321@users.noreply.github.com>
2026-06-30 22:08:05 +0700

    chore(migration): add Challenge table

---
+++ backend/Asterisk.MyTek/Entity/Challenge.cs
@@ -0,0 +1,8 @@
+﻿namespace Asterisk.MyTek.Entity;
+
+public class Challenge
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string CdnUrl { get; set; }
+}
---
+++ backend/Asterisk.MyTek/Migrations/20260630145624_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Asterisk.MyTek.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Challenges",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CdnUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Challenges", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Challenges");
+        }
+    }
+}
