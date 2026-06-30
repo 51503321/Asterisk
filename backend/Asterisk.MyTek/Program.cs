e3dee47ec477c208951992c906334465d1387c9f
Thien <33951318+51503321@users.noreply.github.com>
2026-06-30 22:01:29 +0700

    update

--- backend/Asterisk.MyTek/Program.cs
+++ backend/Asterisk.MyTek/Program.cs
@@ -1,4 +1,4 @@
-using Asterisk.MyTek.HackerRank;
+using Microsoft.EntityFrameworkCore;
 
 namespace Asterisk.MyTek;
 
@@ -7,11 +7,25 @@ public class Program
     public static void Main(string[] args)
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        // adds the necessary services for web API controllers to your application.
+        builder.Services.AddControllers();
+
+        builder.Services.AddDbContext<MyDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        // Adds services required to generate the Swagger / OpenAPI specification document
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
         WebApplication app = builder.Build();
-        app.MapGet("/", () =>
+        if (app.Environment.IsDevelopment())
         {
-            Solution.MainSolution();
-        });
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.MapControllers(); // MapControllers configures the web API controller actions in your app as endpoints
         app.Run();
     }
 }
